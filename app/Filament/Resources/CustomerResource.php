1a6e84ed383987cc2026076c8d59f9e264278130
May <mayzin.o@eqho.com>
2023-02-08 11:32:46 +0700

    create admin panel and policy

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -12,51 +12,69 @@ use Filament\Resources\Table;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Card;
 
 class CustomerResource extends Resource
 {
-    protected static ?string $model = Customer::class;
+	protected static ?string $model = Customer::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+	protected static ?string $navigationIcon = 'heroicon-o-collection';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                //
-            ]);
-    }
+	public static function form(Form $form): Form
+	{
+		return $form
+			->schema([
+				Forms\Components\TextInput::make('name')->required(),
+				Forms\Components\TextInput::make('email')->email()->required()->unique(ignoreRecord: true),
+				Forms\Components\TextInput::make('phone')->required(),
+				Forms\Components\TextInput::make('address')->required(),
+				Select::make('status')
+					->options([
+						'draft'     => 'Draft',
+						'reviewing' => 'Reviewing',
+						'published' => 'Published',
+					]),
+			]);
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
-            ]);
-    }
-    
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
-    
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListCustomers::route('/'),
-            'create' => Pages\CreateCustomer::route('/create'),
-            'edit' => Pages\EditCustomer::route('/{record}/edit'),
-        ];
-    }    
+
+	}
+
+	public static function table(Table $table): Table
+	{
+		return $table
+			->columns([
+				Tables\Columns\TextColumn::make('id'),
+				Tables\Columns\TextColumn::make('name'),
+				Tables\Columns\TextColumn::make('email'),
+				Tables\Columns\TextColumn::make('phone'),
+				Tables\Columns\TextColumn::make('address'),
+			])
+			->filters([
+				//
+			])
+			->actions([
+				Tables\Actions\EditAction::make(),
+			])
+			->bulkActions([
+				Tables\Actions\DeleteBulkAction::make(),
+			]);
+	}
+
+	public static function getRelations(): array
+	{
+		return [
+			//
+		];
+	}
+
+	public static function getPages(): array
+	{
+		return [
+			'index'  => Pages\ListCustomers::route('/'),
+			'create' => Pages\CreateCustomer::route('/create'),
+			'edit'   => Pages\EditCustomer::route('/{record}/edit'),
+			'view'   => Pages\ViewCustomer::route('/{record}'),
+		];
+	}
 }
